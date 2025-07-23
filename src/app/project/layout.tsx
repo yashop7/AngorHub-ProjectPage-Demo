b17810c62f7224372572add4f2e51e9f56bc0c5a
Yash <139779840+yashop7@users.noreply.github.com>
2025-07-23 17:52:15 +0530

    project

---
+++ src/app/project/layout.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from "react"
+
+export default function ProjectLayout({ children }: { children: ReactNode }) {
+  return (
+    <section>
+        
+      {children}
+    </section>
+  )
+}
